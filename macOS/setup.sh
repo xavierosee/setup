b7096c5f113693d456d621854365c978ff254748
Xavier Rosee <xrosee@live.cn>
2021-09-19 22:41:11 +0200

    :art: formatting

--- macOS/setup.sh
+++ macOS/setup.sh
@@ -135,7 +135,6 @@ pyenv install $(pyenv install --list | grep -v - | grep -v rc | grep -v forge | 
 
 # OH-my-zsh & Powerlevel10k
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # git configuration
