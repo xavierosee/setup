d9cd874880d9a449c2990fb0045c8e9de539e831
Xavier Rosee <xrosee@live.cn>
2021-09-19 23:41:53 +0200

    :wrench: add dotfiles

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -82,10 +82,14 @@ bash -c "$(wget -q -O - https://linux.kite.com/dls/linux/current)"
 
 # Install Google Cloud CLI
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo apt-get install apt-transport-https ca-certificates gnupg
+sudo apt-get install -y apt-transport-https ca-certificates gnupg
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk
-sudo apt-get install google-cloud-sdk-app-engine-python
+sudo apt-get update && sudo apt-get install -y google-cloud-sdk google-cloud-sdk-app-engine-python
+
+# Install dotfiles
+git clone --recurse-submodules https://github.com/xavierosee/dotfiles
+mv dotfiles ~/.dotfiles
+cd ~/.dotfiles && ./install.sh
 
 
 # Install pyenv
