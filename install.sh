47ae8fdfc0b2621d7b9e13bd6b8493875e34b334
Xavier Rosee <xrosee@live.cn>
2021-09-21 16:54:51 +0200

    :speech_bubble: increase readability of stdio statements

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install pre-requisites
-echo "Installing required packages"
+echo " ##### Installing required packages ##### "
 case $OSTYPE in
     darwin*)
         # Install HomeBrew (automatically installs Xcode CLT)
@@ -14,7 +14,7 @@ case $OSTYPE in
         sudo apt install -y git
     ;;
 esac
-echo "Required packages successfully installed"
+echo " ##### Required packages successfully installed ##### "
 
 # checkout the repo
 if [ -z "$SETUP_LOC" ]; then
@@ -27,9 +27,9 @@ else
   GITHUB="https://github.com"
 fi
 
-echo "Cloning the repo"
+echo " #####  Cloning the repo ##### "
 git clone --recurse-submodules "${GITHUB}/xavierosee/setup.git" "${SETUP_LOC}"
-echo "Repo cloned to ~/.setup/"
+echo " #####  Repo cloned to ~/.setup/ ##### "
 
 
 # Run OS-specific commands
@@ -43,7 +43,7 @@ case $OSTYPE in
 esac
 
 # Install VS Code extensions
-echo "Installing VS Code extensions"
+echo " #####  Installing VS Code extensions  ##### "
 code --install-extension ms-vscode.sublime-keybindings
 code --install-extension emmanuelbeziat.vscode-great-icons
 code --install-extension MS-vsliveshare.vsliveshare
@@ -87,45 +87,45 @@ code --install-extension sbsnippets.pytorch-snippets
 code --install-extension vense.rails-snippets
 code --install-extension cyrusfirheir.twee3-language-tools
 code --install-extension redhat.vscode-yaml
-echo "VSCode Extensions installed"
+echo " #####  VSCode Extensions installed  ##### "
 
 # zsh, OH-my-zsh & Powerlevel10k
 rm -rf ~/.oh-my-zsh
-echo "Installing oh)my-zsh"
+echo " #####  Installing oh)my-zsh  ##### "
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "oh-my-zsh installed, now cloning p10k and plugins"
+echo " #####  oh-my-zsh installed, now cloning p10k and plugins  ##### "
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # git configuration
-echo "configuring git"
+echo " #####  configuring git  ##### "
 git config --global user.email "xrosee@live.cn"
 git config --global user.name "Xavier Rosée"
 sudo git config --system core.editor "vim"
-echo "git configured, do not forget to gh auth login at the end of all this"
+echo " ##### git configured, do not forget to gh auth login at the end of all this ##### "
 
 # Install dotfiles
-echo "installing dotfiles"
+echo " ##### installing dotfiles ##### "
 git clone --recurse-submodules https://github.com/xavierosee/dotfiles
 mv dotfiles ~/.dotfiles
 cd ~/.dotfiles && ./install.sh
 
 # Install Python (latest) and set as global environment
-echo "installing latest Python version"
+echo " ##### installing latest Python version ##### "
 pyenv install $(pyenv install --list | grep -v - | grep -v rc | grep -v forge | tail -1)
-echo "setting this version as global virtualenv"
+echo " ##### setting this version as global virtualenv ##### "
 pyenv global $(pyenv versions | tail -1)
 
 # save the useful pictures
-echo "saving pictures to the Pictures folder"
+echo " ##### saving pictures to the Pictures folder ##### "
 cp $SETUP_LOC/avatar.jpg ~/Pictures/
 cp $SETUP_LOC/term_bg.jpg ~/Pictures/
 
 # delete all installation files
-echo "cleaning up..."
+echo " ##### cleaning up... ##### "
 rm -rf ~/.dotfiles
 rm -rf $SETUP_LOC
 
-echo "Congratulations! Your Cloudsdale setup is now over."
-echo "This computer will restart in 60 seconds, just as a precaution"
+echo " ##### Congratulations! Your Cloudsdale setup is now over. ##### "
+echo " ##### This computer will restart in 60 seconds, just as a precaution ##### "
 sudo shutdown -r +1
--- linux/setup.sh
+++ linux/setup.sh
@@ -1,71 +1,71 @@
 #!/bin/bash
 
 # change hostname
-echo "Changing HostName"
+echo " ##### Changing HostName ##### "
 hostnamectl set-hostname cloudsdale
 hostnamectl set-hostname "Cloudsdale" --pretty
-echo "HostName successfully changed"
+echo " ##### HostName successfully changed ##### "
 
 # upgrade everything
-echo "upgrading all packages"
+echo " ##### upgrading all packages ##### "
 sudo apt update
 sudo apt upgrade -y
-echo "packages successfully upgraded"
+echo " ##### packages successfully upgraded ##### "
 
 # setup basic dev tools
-echo "installing basic dev tools"
+echo " ##### installing basic dev tools ##### "
 sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git apt-transport-https ca-certificates  software-properties-common guake snapd snap-store
 sudo apt install -y zsh curl vim imagemagick jq
-echo "installing heroku"
+echo " ##### installing heroku ##### "
 sudo snap install heroku --classic
-echo "installing vscode"
+echo " ##### installing vscode ##### "
 sudo snap install code --classic
-echo "installing github cli"
+echo " ##### installing github cli ##### "
 sudo snap install gh
 
 
 # install fonts for PowerLevel10k
-echo "installing Nerd Fonts"
+echo " ##### installing Nerd Fonts ##### "
 rm -rf ~/.fonts
 cp -r ~/.setup/linux/fonts ~/.fonts
-echo "Nerd Fonts successfully installed"
+echo " ##### Nerd Fonts successfully installed ##### "
 
 # install Docker
-echo "installing docker"
+echo " ##### installing docker ##### "
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
 apt-cache policy docker-ce
 sudo apt install -y docker-ce
 sudo usermod -aG docker ${USER}
-echo "Docker successfully installed"
+echo " ##### Docker successfully installed ##### "
 
 # Install Kite
-echo "installing Kite"
+echo " ##### installing Kite ##### "
 bash -c "$(wget -q -O - https://linux.kite.com/dls/linux/current)"
-echo "Kite successfully installed"
+echo " ##### Kite successfully installed ##### "
 
 # Install Google Cloud CLI
-echo "installing Google Cloud CLI"
+echo " ##### installing Google Cloud CLI ##### "
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt-get install -y apt-transport-https ca-certificates gnupg
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install -y google-cloud-sdk google-cloud-sdk-app-engine-python
-echo "Google Cloud Platform SDK successfully installed"
+echo " ##### Google Cloud Platform SDK successfully installed ##### "
 
 # Install NVM
-echo "installing NVM"
+echo " ##### installing NVM ##### "
 rm -rf ~/.nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.zshrc
 nvm install 14.17.6
 nvm alias default node
-echo "NVM successfully installed, default node set to latest node version on system"
+echo " ##### NVM successfully installed, default node set to latest node version on system ##### "
 
 # Install pyenv
 rm -rf ~/.pyenv
 curl https://pyenv.run/ | bash
-echo "PyEnv successfully installed"
+echo " ##### PyEnv successfully installed ##### "
 
 # Install various software and utilities
 ## First add the repositories
@@ -87,16 +87,16 @@ sudo apt install -y tree
 sudo apt install -y mysql-client
 sudo apt install -y postgresql-client
 sudo apt install -y openjdk-11-jdk
-echo "installing postman"
+echo " ##### installing postman ##### "
 sudo snap install postman
-echo "installing brave"
+echo " ##### installing brave ##### "
 sudo snap install brave
 sudo apt install -y speedtest-cli
 sudo apt install -y protonvpn
-echo "installing ferdi"
+echo " ##### installing ferdi ##### "
 sudo snap install ferdi
 sudo apt install -y 1password
-echo "installing Apple Music"
+echo " ##### installing Apple Music ##### "
 sudo snap install apple-music-for-linux
 
 ### Google Drive sync
