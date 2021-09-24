8cb6c5e49749610f434c710a20071ed3ef5be1e3
Xavier Rosee <xrosee@live.cn>
2021-09-24 17:10:13 +0000

    :adhesive_bandage: replace brave by firefox

--- linux/setup.sh
+++ linux/setup.sh
@@ -88,10 +88,9 @@ sudo apt install -y mysql-client
 sudo apt install -y postgresql-client
 sudo apt install -y openjdk-11-jdk
 sudo apt install -y dotnet-sdk-5.0
+sudo apt install -y firefox
 echo " ##### installing postman ##### "
 sudo snap install postman
-echo " ##### installing brave ##### "
-sudo snap install brave
 sudo apt install -y speedtest-cli
 sudo apt install -y protonvpn
 echo " ##### installing ferdi ##### "
